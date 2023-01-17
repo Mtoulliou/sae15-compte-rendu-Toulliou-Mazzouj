899461ac93a028f48ad8c34a0e9881c70e4bc155
Mtoulliou <matteo.meze@gmail.com>
2023-01-17 19:18:12 +0100

    Création du programme de récolte de donné parking voiture

--- main/collecte_voiture.py
+++ main/collecte_voiture.py
@@ -1,29 +1,7 @@
 from lxml import etree
 import time
 import requests
-import matplotlib.pyplot as plt
 import os
-
-import calcul
-"""
-T = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23]
-L1 = [3,3,4,3,2,5,8,9,13,16,18,18,19,21,22,22,21,17,17,12,10,8,7,4] 
-L2 = [103,203,4,3,2,5,8,9,13,16,18,18,19,21,22,22,21,17,17,12,10,-92,-93,-96]
-
-print(f'Moyenne L1 : ',calcul.moyenne(L1))
-print(f'Ecart Type L1 : ',calcul.ecart_type(L1))
-print(f'Moyenne L2 : ',calcul.moyenne(L2))
-print(f'Ecart Type L2 : ',calcul.ecart_type(L2))
-
-'''
-Graphique L1,L2
-'''
-
-calcul.graph(T,L1,L2,"temps(h)","température (°C)")
-#plt.show()
-"""
-
-#######################################################################################
 
 parkings=['FR_MTP_ANTI','FR_MTP_COME','FR_MTP_CORU','FR_MTP_EURO','FR_MTP_FOCH','FR_MTP_GAMB','FR_MTP_GARE','FR_MTP_TRIA','FR_MTP_ARCT','FR_MTP_PITO','FR_MTP_CIRC','FR_MTP_SABI','FR_MTP_GARC','FR_MTP_MOSS','FR_STJ_SJLC','FR_MTP_MEDC','FR_MTP_OCCI','FR_MTP_GA109','FR_MTP_GA250','FR_CAS_CDGA','FR_MTP_ARCE','FR_MTP_POLY'] 
 donnees=['DateTime','Name','Status','Free','Total']
@@ -46,26 +24,47 @@ def fichier_parking(parking) :
     f1.close()
 
 def unxml(donnee,fichier_parking) :
+    '''
+    Fonction permetant de retourner le contenue de la balise xml du fichier choisis
+    '''
     tree = etree.parse(fichier_parking)
     for user in tree.xpath(donnee) :
         return user.text
-#######################################################
-x=0
+
+
+'''
+Programme de récolte d'information des parking de la ville de montpellier.
+'''
+
 for parking in parkings :
     for donnee in donnees :
         name=parking+"."+donnee+'.txt'
-        f1="f"+parking+donnee
         f1=open(name,'w',encoding='utf8')
+        f1.close()
 
-while x!=2 : 
+l=0
+stop = time.time()
+while time.time() != stop-1 : 
     for parking in parkings :
         fichier_parking(parking)
         for donnee in donnees :
-            f1.write(unxml(donnee,parking+'.txt'))
-    time.sleep(3)
-    x+=1
-    
-for parking in parkings :
-    for donnee in donnees :
-        f1="f"+parking+donnee
-        f1.close()
+            name=parking+"."+donnee+'.txt'
+            f1=open(name,'r')
+            f1.close()
+            if os.path.getsize(name) == 0:
+                f1=open(name,'w',encoding='utf8')
+                f1.write(unxml(donnee,parking+'.txt')+"\n")
+                f1.close()
+            else : 
+                f1=open(name,'r')
+                L=f1.readlines()
+                f1.close()
+                L.insert(l,unxml(donnee,parking+'.txt')+"\n")
+                f1=open(name,'w',encoding='utf8')
+                f1.writelines(L)
+                f1.close()
+    l+=1
+    time.sleep(0) 
+
+
+print (f'TERMINE')
