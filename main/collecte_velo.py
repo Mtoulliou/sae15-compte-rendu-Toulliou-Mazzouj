99a5af126b8f6e56a51ef15e752bff0aa82bbdaf
Mtoulliou <matteo.meze@gmail.com>
2023-01-17 19:26:14 +0100

    Création du programme de récolte de donné parking voiture

--- main/collecte_velo.py
+++ main/collecte_velo.py
@@ -1,3 +1,22 @@
 import time
 import requests
 import os
+
+def donnee_parking(parking) :
+    '''
+    Fonction permettant de recuperer les données xml d'un parking renseigné.
+    '''
+    url_param= "https://data.montpellier3m.fr/sites/default/files/ressources/"+parking+".xml"
+    response=requests.get(url_param)
+    return response.text
+
+def fichier_parking(parking) :
+    '''
+    Fonction permettant de créer un fichier contenant les données xml du parking mentionné
+    '''
+    nom_fichier=parking+".txt"
+    f1=open(nom_fichier,"w",encoding='utf8')
+    f1.write(donnee_parking(parking))
+    f1.close()
+
+def unjson(donnee,fichier_parking) :
