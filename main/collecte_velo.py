bf35cd71898752daeadad45494867122677fc41d
Mtoulliou <matteo.meze@gmail.com>
2023-01-18 19:32:57 +0100

    Fin programme récolte velo + voiture

--- main/collecte_velo.py
+++ main/collecte_velo.py
@@ -1,22 +1,57 @@
 import time
 import requests
 import os
+import json
 
-def donnee_parking(parking) :
-    '''
-    Fonction permettant de recuperer les données xml d'un parking renseigné.
-    '''
-    url_param= "https://data.montpellier3m.fr/sites/default/files/ressources/"+parking+".xml"
-    response=requests.get(url_param)
-    return response.text
+parkings=["Rue Jules Ferry - Gare Saint-Roch","Comédie","Esplanade","Hôtel de Ville","Corum","Place Albert 1er - St Charles","Foch","Halles Castellane","Observatoire","Rondelet","Plan Cabanes","Boutonnet","Emile Combes","Beaux-Arts","Les Aubes","Antigone centre","Médiathèque Emile Zola","Nombre d'Or","Louis Blanc","Gambetta","Port Marianne","Clemenceau","Les Arceaux","Cité Mion","Nouveau Saint-Roch","Renouvier","Odysseum","Saint-Denis","Richter","Charles Flahault","Voltaire","Prés d'Arènes","Garcia Lorca","Vert Bois","Malbosc","Occitanie","FacdesSciences","Fac de Lettres","Aiguelongue","Jeu de Mail des Abbés","Euromédecine","Marie Caizergues","Sabines","Celleneuve","Jardin de la Lironde","Père Soulas","Place Viala","Hôtel du Département","Tonnelles","Parvis Jules Ferry - Gare Saint-Roch","Pont de Lattes - Gare Saint-Roch","Deux Ponts - Gare Saint-Roch","Providence - Ovalie","Pérols Etang de l'Or","Albert 1er - Cathédrale","Saint-Guilhem - Courreau","Sud De France"]
+donnees=["num_bikes_available","num_bikes_disabled","num_docks_available","is_installed","is_renting","is_returning"]
 
-def fichier_parking(parking) :
+def fichier_parking() :
     '''
-    Fonction permettant de créer un fichier contenant les données xml du parking mentionné
+    Fonction permettant de recuperer les données json des parkings a vélo et les inscrit dans un fichier json.
     '''
-    nom_fichier=parking+".txt"
-    f1=open(nom_fichier,"w",encoding='utf8')
-    f1.write(donnee_parking(parking))
+    response=requests.get('https://montpellier-fr-smoove.klervi.net/gbfs/en/station_status.json')
+    f1=open("station_status.json","w",encoding='utf8')
+    f1.write(response.text)
     f1.close()
 
-def unjson(donnee,fichier_parking) :
+
+
+'''
+Programme de récolte d'information des parking vélo de la ville de montpellier.
+'''
+
+for parking in parkings :
+    for donnee in donnees :
+        nom_fichier=parking+'.'+donnee+'.txt'
+        f1=open(nom_fichier,"w",encoding='utf8')
+        f1.close()
+
+stop = time.time()
+while time.time() != -1 :
+    for parking in parkings :
+        fichier_parking()
+        fileObject = open("station_status.json","r")
+        jsonContent = fileObject.read()
+        a = json.loads(jsonContent)
+        x=0
+        for donnee in donnees :
+            nom_fichier=parking+'.'+donnee+'.txt'
+            f1=open(nom_fichier,'r')
+            f1.close()
+            if os.path.getsize(nom_fichier) == 0:
+                f1=open(nom_fichier,"w",encoding='utf8')
+                f1.write(str(a['data']['stations'][x][donnee])+'\n')
+                f1.close()
+            else : 
+                f1=open(nom_fichier,'r')
+                L=f1.readlines()
+                f1.close()
+                L.insert(l,str(a['data']['stations'][x][donnee])+'\n')
+                f1=open(nom_fichier,'w',encoding='utf8')
+                f1.writelines(L)
+                f1.close()
+        x+=1
+    time.sleep(300)
+
+print(f'TERMINE')
--- main/collecte_voiture.py
+++ main/collecte_voiture.py
@@ -1,7 +1,7 @@
-from lxml import etree
 import time
 import requests
 import os
+from lxml import etree
 
 parkings=['FR_MTP_ANTI','FR_MTP_COME','FR_MTP_CORU','FR_MTP_EURO','FR_MTP_FOCH','FR_MTP_GAMB','FR_MTP_GARE','FR_MTP_TRIA','FR_MTP_ARCT','FR_MTP_PITO','FR_MTP_CIRC','FR_MTP_SABI','FR_MTP_GARC','FR_MTP_MOSS','FR_STJ_SJLC','FR_MTP_MEDC','FR_MTP_OCCI','FR_MTP_GA109','FR_MTP_GA250','FR_CAS_CDGA','FR_MTP_ARCE','FR_MTP_POLY'] 
 donnees=['DateTime','Name','Status','Free','Total']
@@ -44,7 +44,7 @@ for parking in parkings :
 
 l=0
 stop = time.time()
-while time.time() != stop-1 : 
+while time.time() != -1 : 
     for parking in parkings :
         fichier_parking(parking)
         for donnee in donnees :
@@ -63,8 +63,8 @@ while time.time() != stop-1 :
                 f1=open(name,'w',encoding='utf8')
                 f1.writelines(L)
                 f1.close()
-    l+=1
-    time.sleep(0) 
+        l+=1
+    time.sleep(300) 
 
 
 print (f'TERMINE')
